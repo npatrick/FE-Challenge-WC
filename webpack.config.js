1dfe1380e6d04927a032d4931f989989938f9a34
Neil Romana <wazzupnickz@gmail.com>
2017-08-09 16:17:17 -0700

    add react, webpack, and babel

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack'); //to access built-in plugins
+
+module.exports = {
+	devtool: 'source-map',
+	entry: [
+		'./src/client/index.js'
+	],
+	output: {
+		path: __dirname + '/src/public',
+		publicPath: '/src/public/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+        exclude: /node_modules/, //exclude any and all files in the node_modules folder
+        loader: 'babel-loader',
+        query: {
+					presets: ['es2015', 'react', 'env']
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ] 
+			}
+		]
+	},
+	devServer: {
+		port: 3000,
+		inline: true
+	}
+};
