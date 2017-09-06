6000bad8162c01419b3dd1e9cac932c89c37d1b2
Neil Romana <wazzupnickz@gmail.com>
2017-09-05 22:40:01 -0700

    fix default flag size

--- src/client/components/table_list_item.react.jsx
+++ src/client/components/table_list_item.react.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 const TableListItem = (props) => {
+  const tableStyle = {
+    'width': '53',
+    'height': 'auto'
+  }
   return (
     <tr>
-      <td><img className='flag-pics' src={props.flag} /></td>
+      <td><img className='flag-pics' src={props.flag} style={tableStyle}/></td>
       <td>{props.name}</td>
       <td>{props.countryCode}</td>
       <td>{props.continent}</td>
